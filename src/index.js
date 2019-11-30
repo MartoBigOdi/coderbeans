ea89cb873d1a9c3a9cc25c1f8e56375c7dc418ee
martin_vasconcelo <Marto Pc@MartoVasc.local>
2019-11-30 17:32:57 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ require('../src/database')
 //Desde Express() conseguimos app por eso lo metemos en config().
 const app = configs(express());
 
- 
 app.listen(app.get('port'), () => {
     console.log('Server on Port'.rainbow.italic, app.get('port'));
 });
